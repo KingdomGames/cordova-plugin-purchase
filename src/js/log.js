92d87bb221c8e5e145cc514b9b6d0a81715846ea
Andy Terranova <aterranova@kingdomgames.com>
2015-02-23 20:42:42 -0600

    POST logger

--- src/js/log.js
+++ src/js/log.js
@@ -16,7 +16,7 @@ function log(level, o) {
         o = JSON.stringify(o);
 
     //send to our server
-    $.ajax("192.168.50.37:4444", {
+    $.ajax("192.168.50.37:4444/logger", {
         type: "POST",
         data: {
             "level": level,
